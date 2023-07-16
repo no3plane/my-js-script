cd986ca9af52656fc074af1f4cc6958ac23c2580
Solstice <no3plane@yeah.net>
2023-07-16 23:08:26 +0800

    feat: 控制台输出显示时间戳

--- 读取任务管理器数据/ConsolePrinter.py
+++ 读取任务管理器数据/ConsolePrinter.py
@@ -1,4 +1,5 @@
-from ProcRow import ProcRow, ProcCol
+from ProcRow import ProcCol
+from datetime import datetime
 
 
 class ConsolePrinter:
@@ -12,9 +13,10 @@ class ConsolePrinter:
         self._printValue(cols)
 
     def _printValue(self, cols: list[ProcCol]):
-        procValues = [col.value for col in cols]
+        timestamp = datetime.now().strftime("%M:%S:%f")[:-3]
+        procValues = [timestamp] + [col.value for col in cols]
         print("\t".join(procValues))
 
     def _printTitle(self, cols: list[ProcCol]):
-        procTitles = [infoItem.name for infoItem in cols]
+        procTitles = ["时间戳"] + [infoItem.name for infoItem in cols]
         print("\t".join(procTitles))
